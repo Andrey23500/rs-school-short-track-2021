3394ca061254875be04beaabe5bc65362eafc6ee
Andrey23500 <andreyavs.00@gmail.com>
2021-04-09 17:26:42 +0300

    init: start short-track-2021 task

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,18 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+
+  const colCount = matrix[0].length;
+  for (let j = 0; j < colCount; j++) {
+    for (let i = 0; i < matrix.length; i++) {
+      if (matrix[i][j] !== 0) sum += matrix[i][j];
+      else break;
+    }
+  }
+
+  return sum;
 }
 
 module.exports = getMatrixElementsSum;
